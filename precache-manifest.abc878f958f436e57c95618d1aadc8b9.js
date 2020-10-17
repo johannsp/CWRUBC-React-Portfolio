be2cc7386b0ba019026e88ff4952ae9748fff523
John L Spetz <JohnLSpetz@zoho.com>
2020-10-17 14:55:15 -0400

    Updates

---
+++ precache-manifest.abc878f958f436e57c95618d1aadc8b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15820dab9f44df6df34f009634e9c7e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a22d5ceec168397e6d91",
+    "url": "/static/css/main.61169a3c.chunk.css"
+  },
+  {
+    "revision": "08bc8fc6f6ccfd922f02",
+    "url": "/static/js/2.237b843a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.237b843a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a22d5ceec168397e6d91",
+    "url": "/static/js/main.ae88e03e.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "d450e58fe24fea886a88c491e745e326",
+    "url": "/static/media/green_sky_4-wallpaper-_01_1388x1080.d450e58f.png"
+  }
+]);
