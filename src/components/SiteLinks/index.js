406b575164a3239ba0558cdd8d0802ade86e329a
John L Spetz <JohnLSpetz@zoho.com>
2020-10-19 18:28:40 -0400

    Fix bad basename in link

--- src/components/SiteLinks/index.js
+++ src/components/SiteLinks/index.js
@@ -94,14 +94,14 @@ export function SiteLinks() {
         <SiteLinkRow
           icon={FaHome}
           section="Home"
-          linkType="/CWRUBC-React-Profile/"
+          linkType="/CWRUBC-React-Portfolio/"
           link="home"
           blank={false}
         >Profile Page</SiteLinkRow>
         <SiteLinkRow
           icon={FaTv}
           section="Portfolio"
-          linkType="/CWRUBC-React-Profile/"
+          linkType="/CWRUBC-React-Portfolio/"
           link="Portfolio"
           blank={false}
         >Recent Projects</SiteLinkRow>
