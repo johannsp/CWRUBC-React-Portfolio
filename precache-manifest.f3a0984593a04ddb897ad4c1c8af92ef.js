7019a13807c50b545f6297381afa360f2d86b7f3
John L Spetz <JohnLSpetz@zoho.com>
2020-10-19 18:17:32 -0400

    Updates

---
+++ precache-manifest.f3a0984593a04ddb897ad4c1c8af92ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23b684e5f65f4311bce566e79ef28f7",
+    "url": "/CWRUBC-React-Portfolio/index.html"
+  },
+  {
+    "revision": "97111caae9ad90563434",
+    "url": "/CWRUBC-React-Portfolio/static/css/main.84b64041.chunk.css"
+  },
+  {
+    "revision": "08bc8fc6f6ccfd922f02",
+    "url": "/CWRUBC-React-Portfolio/static/js/2.237b843a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CWRUBC-React-Portfolio/static/js/2.237b843a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97111caae9ad90563434",
+    "url": "/CWRUBC-React-Portfolio/static/js/main.c9d14505.chunk.js"
+  },
+  {
+    "revision": "07ad30cc1402cc89d94c",
+    "url": "/CWRUBC-React-Portfolio/static/js/runtime-main.2e713e27.js"
+  },
+  {
+    "revision": "d450e58fe24fea886a88c491e745e326",
+    "url": "/CWRUBC-React-Portfolio/static/media/green_sky_4-wallpaper-_01_1388x1080.d450e58f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f10d27b19cfd1289b48088400256ccf.js"
+  "/CWRUBC-React-Portfolio/precache-manifest.f3a0984593a04ddb897ad4c1c8af92ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CWRUBC-React-Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
